fea87a9a9062aa44036127f6bedb2659cdc9dfe7
philanderson888 <philanderson888@hotmail.com>
2021-08-28 10:53:21 +0100

    update api logic for todos

--- routes/todos.js
+++ routes/todos.js
@@ -5,12 +5,8 @@ const jwt = require("jsonwebtoken");
 const { SECRET } = require("../constants");
 
 const verifyToken = (req, res, next) => {
-  console.log(`verifying token`)
   try {
-    console.log(`jwt authorises header ${req.headers.authorization} with secret ${SECRET}`)
     req.user = jwt.verify(req.headers.authorization, SECRET);
-    console.log(`request user = `)
-    console.log(req.user)
     return next();
   } catch (err) {
     console.log(err);
@@ -19,11 +15,8 @@ const verifyToken = (req, res, next) => {
 };
 
 router.get("/", verifyToken, async (req, res) => {
-  console.log(`request to root folder for user`)
-  console.log(req.user)
   const { _id } = req.user;
   const todos = await Todo.find({ user: _id });
-  console.log(`todos found for user = ${todos}`)
   res.json(todos);
 });
 
@@ -31,15 +24,13 @@ router.get("/:id", verifyToken, async (req, res) => {
   const { _id } = req.user;
   const { id } = req.params;
   const todo = await Todo.findOne({ _id: id, user: _id });
-  console.log(`finding one to do item ${todo}`)
   res.json(todo);
 });
 
 router.post("/", verifyToken, async (req, res) => {
-  const { name, done } = req.body;
+  const { name } = req.body;
   const { _id } = req.user;
-  const todo = new Todo({ name, done, user: _id });
-  console.log(`creating new todo item ${todo}`)
+  const todo = new Todo({ name, done: false, user: _id });
   await todo.save();
   res.json(todo);
 });
