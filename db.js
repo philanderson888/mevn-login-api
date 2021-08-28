39438628ab187ae589ff49748b59adb8cbfd247b
philanderson888 <philanderson888@hotmail.com>
2021-08-28 10:54:23 +0100

    add connection to mongodb atlas online

--- db.js
+++ db.js
@@ -1,5 +1,9 @@
 const { Schema, createConnection } = require('mongoose');
-const connection = createConnection('mongodb://localhost:27017/mevn-example', { useNewUrlParser: true, useUnifiedTopology: true });
+const dotenv = require('dotenv').config().parsed
+console.log(`mongo db username is ${process.env.VUE_APP_MONGO_DB_USERNAME}`)
+console.log(`mongo db password is ${process.env.VUE_APP_MONGO_DB_PASSWORD}`)
+const connection2 = createConnection('mongodb://localhost:27017/mevn-example', { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false });
+const connection = createConnection(`mongodb+srv://${process.env.VUE_APP_MONGO_DB_USERNAME}:${process.env.VUE_APP_MONGO_DB_PASSWORD}@cluster0.iilta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
 
 const userSchema = new Schema({
   name: String,
